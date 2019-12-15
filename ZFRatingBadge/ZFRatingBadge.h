a2aee0248b3ce173aa2bbe41bc7d99d176d464ae
zafarivaev <z.ivaev@mail.ru>
2019-12-15 10:47:58 +0500

    Initial Commit

---
+++ ZFRatingBadge/ZFRatingBadge.h
@@ -0,0 +1,19 @@
+//
+//  ZFRatingBadge.h
+//  ZFRatingBadge
+//
+//  Created by Zafar on 12/15/19.
+//  Copyright © 2019 Zafar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZFRatingBadge.
+FOUNDATION_EXPORT double ZFRatingBadgeVersionNumber;
+
+//! Project version string for ZFRatingBadge.
+FOUNDATION_EXPORT const unsigned char ZFRatingBadgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFRatingBadge/PublicHeader.h>
+
+
